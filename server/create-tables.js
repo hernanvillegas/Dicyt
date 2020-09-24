20e0accd31ec49e9f1be84b93b7503616a3944f4
Villegas <hernanvillegas000@gmail.com>
2020-09-24 13:19:10 -0400

    second commit

--- server/create-tables.js
+++ server/create-tables.js
@@ -1,7 +1,8 @@
 var server = require('./server');
 var ds = server.dataSources.mysql;
 // var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'user', 'career_institute', 'investigation_project', 'idh-project'];
-var lbTables = ['auxiliar', 'investigator', 'responsable'];
+// var lbTables = ['auxiliar', 'investigator', 'responsable'];
+var lbTables = ['idh-project'];
 ds.automigrate(lbTables, function(er) {
     if (er) throw er;
     console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
